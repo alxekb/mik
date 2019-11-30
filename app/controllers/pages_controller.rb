9c0eb889190601798fe76877dc43e9474ce9a5ae
Mr Ivanov <alx.ekb@gmail.com>
2019-11-30 18:47:34 +0500

    Post (#6)
    * Bump devise from 4.7.0 to 4.7.1
    
    Bumps [devise](https://github.com/plataformatec/devise) from 4.7.0 to 4.7.1.
    - [Release notes](https://github.com/plataformatec/devise/releases)
    - [Changelog](https://github.com/plataformatec/devise/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/plataformatec/devise/compare/v4.7.0...v4.7.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * hotfix(remoce packege-lock.json)
    
    * hotfix(remove packege-lock.json)
    
    * hotfix(fix db adapter)
    
    * hotfix(fix db name)
    
    * feature(post)
    
    Add views and decorator

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
+  decorates_assigned :post
   def index
-    @posts = Post.where('updated_at > ?', 7.days.ago)
+    @posts = Post.where('updated_at > ?', 7.days.ago).decorate
   end
 end
